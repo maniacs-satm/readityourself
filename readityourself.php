6a5062753ec63a12fb8c17174738745666951046
memiks <contact@memiks.fr>
2013-09-04 09:42:06 +0200

    0.0.2

--- readityourself.php
+++ readityourself.php
@@ -20,18 +20,46 @@ function get_external_file($url, $timeout) {
 	// spoofing FireFox 18.0
 	$useragent="Mozilla/5.0 (Windows NT 5.1; rv:18.0) Gecko/20100101 Firefox/18.0";
 
-	// create http context and add timeout and user-agent
-	$context = stream_context_create(array('http'=>array('timeout' => $timeout, // Timeout : time until we stop waiting for the response.
-																				'header'=> "User-Agent: ".$useragent // spoot Mozilla Firefox
-													)));
+	if  (in_array  ('curl', get_loaded_extensions())) {	
+		// Fetch feed from URL
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		 
+		// FeedBurner requires a proper USER-AGENT...
+		curl_setopt($curl, CURL_HTTP_VERSION_1_1, true);
+		curl_setopt($curl, CURLOPT_ENCODING, "gzip, deflate");
+		curl_setopt($curl, CURLOPT_USERAGENT, $useragent);
 
-	// only download page lesser than 4MB
-	$data = @file_get_contents($url, false, $context, -1, 4000000); // We download at most 4 MB from source.
+		$data = curl_exec($curl);
+		
+		$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		
+		$httpcodeOK = isset($httpcode) and ($httpcode == 200 or $httpcode == 301);
+		
+		curl_close($curl);
+	} else {
 
-//	echo "<pre>http_response_header : ".print_r($http_response_header);
-	
+		// create http context and add timeout and user-agent
+		$context = stream_context_create(array('http'=>array('timeout' => $timeout, // Timeout : time until we stop waiting for the response.
+																					'header'=> "User-Agent: ".$useragent, // spoot Mozilla Firefox
+																					'follow_location' => true
+														)));
+
+		// only download page lesser than 4MB
+		$data = @file_get_contents($url, false, $context, -1, 4000000); // We download at most 4 MB from source.
+		//	echo "<pre>http_response_header : ".print_r($http_response_header);
+		
+		if(isset($http_response_header) and isset($http_response_header[0])) {
+			$httpcodeOK = isset($http_response_header) and isset($http_response_header[0]) and ((strpos($http_response_header[0], '200 OK') !== FALSE) or (strpos($http_response_header[0], '301 Moved Permanently') !== FALSE));
+		}
+	}
+
 	// if response is not empty and response is OK
-	if (isset($data) and isset($http_response_header) and isset($http_response_header[0]) and ((strpos($http_response_header[0], '200 OK') !== FALSE) or (strpos($http_response_header[0], '301 Moved Permanently') !== FALSE)) ) {
+	if (isset($data) and isset($httpcodeOK) and httpcodeOK ) {
 
 		// take charset of page and get it
 		preg_match('#<meta .*charset=.*>#Usi', $data, $meta);
@@ -52,7 +80,7 @@ function get_external_file($url, $timeout) {
 		// replace charset of url to charset of page
 		$data = str_replace('charset='.$enc[1], 'charset='.$html_charset, $data);
 
-		return "<iframe>".$data."</iframe>";
+		return $data;
 	}
 	else {
 		return FALSE;
@@ -79,7 +107,6 @@ if(isset($_GET['url']) && $_GET['url'] != null && trim($_GET['url']) != "") {
 		
 			// send result to readability library
 			$r = new Readability($html, $url);
-			
 
 			if($r->init()) {
 				// return innerhtml of article found
