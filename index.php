210c47ac46f92ae464fe87824ebd3efe2073e11e
memiks <contact@memiks.fr>
2013-09-05 16:04:54 +0200

    Change index and add CHANGE

--- index.php
+++ index.php
@@ -14,15 +14,8 @@ function url(){
 			<form action="./readityourself.php">
 				<input type="text" name="url" id="url" maxlength="2048" size="80" /><input type="submit">
 			</form>
-				CHANGE: 
-					2013-03-01 : 
-						* use cURL if php extension is present, otherwise use file_get_contents
-						* now ReadItYourself can handle URL forward
-						* Add a Bookmarklet for <a href="javascript:(function(){var%20url%20=%20location.href;var%20title%20=%20document.title%20||%20url;window.open('<? echo url()?>/readityourself.php?url='%20+%20encodeURIComponent(url),'_self');})();" title="ReadItYourself">ReadItYourself</a> Coded by Tristan Velter for Read It Yourself (thanks ;) ) <a href="http://velter.org/">http://velter.org/</a>
-						* use RainTPL to render html page
-						* add footer to put licence, where to find information about ReadItYourself and version number
 
-
+    		<?php include("./CHANGE"); ?>
 			<?php include("./LICENSE"); ?>
 							 
 		</pre>
