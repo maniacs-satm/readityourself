6179788029bc394c3af1ebe8f0dd1a42115cee86
memiks <contact@memiks.fr>
2014-06-30 09:31:28 +0200

    Check if HTTPS is a server protocol.

--- inc/config.php
+++ inc/config.php
@@ -50,7 +50,7 @@ foreach ($_GET as $name => $value)
 unset($_REQUEST);
 
 function url(){
-  $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+  $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off") ? "https" : "http";
   return $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 }
 
