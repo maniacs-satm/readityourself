fc28fad758819bba87d665ef23dc24a07015d9e5
memiks <contact@memiks.fr>
2014-07-05 21:38:13 +0200

    Changelog

--- class/Readityourself.php
+++ class/Readityourself.php
@@ -2,135 +2,250 @@
 
 class Readityourself extends Readability {
 
-
     private $stripColonIntitle = false;
     private $reduceTitleLength = false;
 
     /**
-	* Get the article title as an H1.
-	*
-	* @return DOMElement
-    *
-	* All of the regular expressions in use within readability.
-	* Defined up here so we don't instantiate them repeatedly in loops.
-	**/
-	public $regexps = array(
-		'unlikelyCandidates' => '/combx|community|comment|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|debutDefinition_/i',
-		'okMaybeItsACandidate' => '/and|post|article|body|column|main|shadow/i',
-		'positive' => '/article|content|comic|body|content|entry|hentry|main|page|media|attachment|pagination|post|text|blog|story/i',
-		'negative' => '/combx|com-|contact|comment|foot|footer|_nav|footnote|masthead|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget/i',
+     * Get the article title as an H1.
+     *
+     * @return DOMElement
+     *
+     * All of the regular expressions in use within readability.
+     * Defined up here so we don't instantiate them repeatedly in loops.
+     * */
+    public $regexps = array(
+        'unlikelyCandidates' => '/combx|community|comment|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|debutDefinition_/i',
+        'okMaybeItsACandidate' => '/and|post|article|body|column|main|shadow/i',
+        'positive' => '/article|content|comic|body|content|entry|hentry|main|page|media|attachment|pagination|post|text|blog|story/i',
+        'negative' => '/combx|com-|contact|comment|foot|footer|_nav|footnote|masthead|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget/i',
         'htmlPositiveTag' => '/content|article|media|video/',
         'htmlNegativeTag' => '/footer|header/',
-		'divToPElements' => '/<(a|blockquote|dl|div|img|ol|p|pre|table|ul|code)/i',
-		'replaceBrs' => '/(<br[^>]*>[ \n\r\t]*){2,}/i',
-		'replaceFonts' => '/<(\/?)font[^>]*>/i',
-		// 'trimRe' => '/^\s+|\s+$/g', // PHP has trim()
-		'normalize' => '/\s{2,}/',
-		'killBreaks' => '/(<br\s*\/?>(\s|&nbsp;?)*){1,}/',
-		'video' => '!//(player\.|www\.)?(youtube|vimeo|viddler)\.com!i',
-		'skipFootnoteLink' => '/^\s*(\[?[a-z0-9]{1,2}\]?|^|edit|citation needed)\s*$/i'
-	);	
-	
-    /**
-    * Get the article title as an H1.
-	*
-	* @return DOMElement
-    * 
-    * Fixes #1 by remove splt ior ":" in title text
-	*/
-	protected function getArticleTitle() {
-		$curTitle = '';
-		$origTitle = '';
+        'divToPElements' => '/<(a|blockquote|dl|div|img|ol|p|pre|table|ul|code)/i',
+        'replaceBrs' => '/(<br[^>]*>[ \n\r\t]*){2,}/i',
+        'replaceFonts' => '/<(\/?)font[^>]*>/i',
+        // 'trimRe' => '/^\s+|\s+$/g', // PHP has trim()
+        'normalize' => '/\s{2,}/',
+        'killBreaks' => '/(<br\s*\/?>(\s|&nbsp;?)*){1,}/',
+        'video' => '!//(player\.|www\.)?(youtube|vimeo|viddler)\.com!i',
+        'skipFootnoteLink' => '/^\s*(\[?[a-z0-9]{1,2}\]?|^|edit|citation needed)\s*$/i'
+    );
 
-		try {
-			$curTitle = $origTitle = $this->getInnerText($this->dom->getElementsByTagName('title')->item(0));
-		} catch(Exception $e) {}
-		
-		if (preg_match('/ [\|\-] /', $curTitle))
-		{
-			$curTitle = preg_replace('/(.*)[\|\-] .*/i', '$1', $origTitle);
-			
-			if (count(explode(' ', $curTitle)) < 3) {
-				$curTitle = preg_replace('/[^\|\-]*[\|\-](.*)/i', '$1', $origTitle);
-			}
-		}
+    /**
+     * Get the article title as an H1.
+     *
+     * @return DOMElement
+     * 
+     * Fixes #1 by remove splt ior ":" in title text
+     */
+    protected function getArticleTitle() {
+        $curTitle = '';
+        $origTitle = '';
+
+        try {
+            $curTitle = $origTitle = $this->getInnerText($this->dom->getElementsByTagName('title')->item(0));
+        } catch (Exception $e) {
+            
+        }
+
+        if (preg_match('/ [\|\-] /', $curTitle)) {
+            $curTitle = preg_replace('/(.*)[\|\-] .*/i', '$1', $origTitle);
+
+            if (count(explode(' ', $curTitle)) < 3) {
+                $curTitle = preg_replace('/[^\|\-]*[\|\-](.*)/i', '$1', $origTitle);
+            }
+        }
 
         // remove to correct split of title with ':' see https://github.com/memiks/readityourself/issues/1
-		else if ($this->stripColonIntitle==true && strpos($curTitle, ': ') !== false)
-		{
-			$curTitle = preg_replace('/.*:(.*)/i', '$1', $origTitle);
+        else if ($this->stripColonIntitle == true && strpos($curTitle, ': ') !== false) {
+            $curTitle = preg_replace('/.*:(.*)/i', '$1', $origTitle);
 
-			if (count(explode(' ', $curTitle)) < 3) {
-				$curTitle = preg_replace('/[^:]*[:](.*)/i','$1', $origTitle);
-			}
-		}
-        
+            if (count(explode(' ', $curTitle)) < 3) {
+                $curTitle = preg_replace('/[^:]*[:](.*)/i', '$1', $origTitle);
+            }
+        }
 
-		else if($this->reduceTitleLength==true && ( strlen($curTitle) > 150 || strlen($curTitle) < 15))
-		{
-			$hOnes = $this->dom->getElementsByTagName('h1');
-			if($hOnes->length == 1)
-			{
-				$curTitle = $this->getInnerText($hOnes->item(0));
-			}
-		}
 
-		$curTitle = trim($curTitle);
+        if (count(explode(' ', trim($curTitle))) <= 4) {
+            $curTitle = $origTitle;
+        } else if ($this->reduceTitleLength == true && ( strlen($curTitle) > 150 || strlen($curTitle) < 15)) {
+            $hOnes = $this->dom->getElementsByTagName('h1');
+            if ($hOnes->length == 1) {
+                $curTitle = $this->getInnerText($hOnes->item(0));
+            }
+        }
 
-		if (count(explode(' ', $curTitle)) <= 4) {
-			$curTitle = $origTitle;
-		}
-		
-		$articleTitle = $this->dom->createElement('h1');
-		$articleTitle->innerHTML = $curTitle;
-		
-		return $articleTitle;
-	}
-	
+        $curTitle = trim($curTitle);
+
+        $articleTitle = $this->dom->createElement('h1');
+        $articleTitle->innerHTML = trim($curTitle);
+
+        return $articleTitle;
+    }
+
     /**
-	* Get an elements class/id weight. Uses regular expressions to tell if this 
-	* element looks good or bad.
-	*
-	* @param DOMElement $e
-	* @return number (Integer)
-	*/
-	public function getClassWeight($e) {
-		if(!$this->flagIsActive(self::FLAG_WEIGHT_CLASSES)) {
-			return 0;
-		}
+     * Get an elements class/id weight. Uses regular expressions to tell if this 
+     * element looks good or bad.
+     *
+     * @param DOMElement $e
+     * @return number (Integer)
+     */
+    public function getClassWeight($e) {
+        if (!$this->flagIsActive(self::FLAG_WEIGHT_CLASSES)) {
+            return 0;
+        }
 
-		$weight = 0;
+        $weight = 0;
 
-    	if (preg_match($this->regexps['negative'], $e->tagName)) {
-			$weight -= 250;
-		}
-		if (preg_match($this->regexps['positive'], $e->tagName)) {
-			$weight += 250;
-		}
+        if (preg_match($this->regexps['negative'], $e->tagName)) {
+            $weight -= 250;
+        }
+        if (preg_match($this->regexps['positive'], $e->tagName)) {
+            $weight += 250;
+        }
 
-    	/* Look for a special ID */
-		if ($e->hasAttribute('id') && $e->getAttribute('id') != '')
-		{
-			if (preg_match($this->regexps['negative'], $e->getAttribute('id'))) {
-				$weight -= 150;
-			}
-			if (preg_match($this->regexps['positive'], $e->getAttribute('id'))) {
-				$weight += 150;
-			}
-		}
+        /* Look for a special ID */
+        if ($e->hasAttribute('id') && $e->getAttribute('id') != '') {
+            if (preg_match($this->regexps['negative'], $e->getAttribute('id'))) {
+                $weight -= 150;
+            }
+            if (preg_match($this->regexps['positive'], $e->getAttribute('id'))) {
+                $weight += 150;
+            }
+        }
 
         /* Look for a special classname */
-		if ($e->hasAttribute('class') && $e->getAttribute('class') != '')
-		{
-			if (preg_match($this->regexps['negative'], $e->getAttribute('class'))) {
-				$weight -= 25;
-			}
-			if (preg_match($this->regexps['positive'], $e->getAttribute('class'))) {
-				$weight += 25;
-			}
-		}
+        foreach ($classes as $class) {
+            if (preg_match($this->regexps['negative'], $class)) {
+                $weight -= 25;
+            }
+            if (preg_match($this->regexps['positive'], $class)) {
+                $weight += 25;
+            }
+        }
 
-		return $weight;
-	}
+        return $weight;
+    }
+
+    protected function grabArticle($page = null) {
+        $articleNode = parent::grabArticle($page);
+
+        $articleNode->innerHTML = $this->modifyContent($articleNode->innerHTML);
+
+        return $articleNode;
+    }
+
+    public function modifyContent($articleContent) {
+        global $PICTURES_DOWNLOAD, $PICTURES_BASE64;
+        $content = $this->absolutes_links($articleContent);
+        if ($PICTURES_DOWNLOAD == true || $PICTURES_BASE64 == true) {
+            $content = $this->picture_filtre($content);
+        }
+        return $content;
+    }
+
+    /**
+     * On modifie les URLS des images dans le corps de l'article
+     */
+    private function picture_filtre($content) {
+        global $PICTURES_DOWNLOAD, $PICTURES_BASE64;
+
+        $matches = array();
+        preg_match_all('#<\s*(img)[^>]+src="([^"]*)"[^>]*>#Si', $content, $matches, PREG_SET_ORDER);
+        foreach ($matches as $i => $link) {
+            $link[1] = trim($link[1]);
+            if (!preg_match('#^(([a-z]+://)|(\#))#', $link[1])) {
+                $absolute_path = $this->rel2abs($link[2]);
+                $filename = basename(parse_url($absolute_path, PHP_URL_PATH));
+                $directory = Utils::create_assets_directory($this->url) . '/' . IMAGES_PATH;
+                if (!is_dir($directory)) {
+                    mkdir($directory, 0705);
+                }
+
+                $fullpath = $directory . '/' . $filename;
+                if ($PICTURES_DOWNLOAD) {
+                    Utils::download_resources($absolute_path, $fullpath);
+                    $content = str_replace($matches[$i][2], $fullpath, $content);
+                } else if ($PICTURES_BASE64) {
+                    $srcb64 = Utils::pictures_base64($absolute_path, $fullpath);
+                    $content = str_replace($matches[$i][2], $srcb64, $content);
+                }
+            }
+        }
+
+        return $content;
+    }
+
+    public function absolutes_links($data) {
+        $doc = new DOMDocument('1.0', 'UTF-8');
+        $doc->encoding = 'UTF-8';
+
+        libxml_use_internal_errors(true);
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $data);
+        libxml_use_internal_errors(false);
+
+        // cherche les balises qui contiennent un href ou un src
+        $docAbsolute = $this->absolute_for_DOM_and_query($doc, '//*/@src | //*/@href');
+
+        return $docAbsolute->saveHTML();
+    }
+
+    public function absolute_for_DOM_and_query($doc, $query) {
+        $xpath = new DOMXPath($doc);
+        $entries = $xpath->query($query);
+
+        if ($entries != null && count($entries) > 0) {
+            foreach ($entries as $entry) {
+                $entry->nodeValue = $this->absolute_for_entry($entry->nodeValue);
+            }
+        }
+
+        return $doc;
+    }
+
+    public function absolute_for_entry($nodevalue) {
+        $nodevalue = htmlentities(html_entity_decode($nodevalue));
+        if (!preg_match('%^((http[s]?://)|(www\.)|(#))([a-z0-9-].?)+(:[0-9]+)?(/.*)?$%', $nodevalue)) {
+            $nodevalue = $this->rel2abs($nodevalue);
+        }
+        return $nodevalue;
+    }
+
+    public function rel2abs($rel) {
+        /* return if already absolute URL */
+        if (parse_url($rel, PHP_URL_SCHEME) != '') {
+            return $rel;
+        }
+
+        /* queries and anchors */
+        if ($rel[0] == '#' || $rel[0] == '?') {
+            return $this->url . $rel;
+        }
+
+        /* parse base URL and convert to local variables:
+          $scheme, $host, $path */
+        $host = "";
+        $scheme = "";
+        extract(parse_url($this->url));
+
+        /* remove non-directory element from path */
+        $path = preg_replace('#/[^/]*$#', '', $path);
+
+        /* destroy path if relative url points to root */
+        if ($rel[0] == '/') {
+            $path = '';
+        }
+
+        /* dirty absolute URL */
+        $abs = "$host$path/$rel";
+
+        /* replace '//' or '/./' or '/foo/../' with '/' */
+        $re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
+        for ($n = 1; $n > 0; $abs = preg_replace($re, '/', $abs, -1, $n)) {
+            
+        }
+
+        /* absolute URL is ready! */
+        return $scheme . '://' . $abs;
+    }
 
 }
--- readityourself.php
+++ readityourself.php
@@ -1,99 +1,98 @@
 <?php
+
 // FUNCTIONS BEGIN
-require_once dirname(__FILE__).'/inc/includes.php';
+require_once dirname(__FILE__) . '/inc/includes.php';
 
 // get readability library
-require_once dirname(__FILE__).'/inc/Readability.php';
+require_once dirname(__FILE__) . '/inc/Readability.php';
 
 // get Encoding library.
-require_once dirname(__FILE__).'/inc/Encoding.php';
+require_once dirname(__FILE__) . '/inc/Encoding.php';
 
 // FUNCTIONS BEGIN
-if(isset($_GET['picdown']) && $_GET['picdown'] != null) {
+if (isset($_GET['picdown']) && $_GET['picdown'] != null) {
     $PICTURES_DOWNLOAD = $_GET['picdown'] == 'true';
 }
 
-if(isset($_GET['picb64']) && $_GET['picb64'] != null) {
+if (isset($_GET['picb64']) && $_GET['picb64'] != null) {
     $PICTURES_BASE64 = $_GET['picb64'] == 'true';
 }
 
-if(isset($_GET['css']) && $_GET['css'] != null && file_exists('css/'.$_GET['css'].'.css')) {
-    $CSS_STYLE = 'css/'.$_GET['css'].'.css';
+if (isset($_GET['css']) && $_GET['css'] != null && file_exists('css/' . $_GET['css'] . '.css')) {
+    $CSS_STYLE = 'css/' . $_GET['css'] . '.css';
 }
 
-function generate_page($url,$title,$content) {
+function generate_page($url, $title, $content) {
     global $CSS_STYLE;
 
-	raintpl::$tpl_dir = './tpl/'; // template directory
-	raintpl::$cache_dir = "./cache/"; // cache directory
-	raintpl::$base_url = url(); // base URL of blog
-	raintpl::configure( 'path_replace', false );
-	raintpl::configure('debug', true); 
+    raintpl::$tpl_dir = './tpl/'; // template directory
+    raintpl::$cache_dir = "./cache/"; // cache directory
+    raintpl::$base_url = url(); // base URL of blog
+    raintpl::configure('path_replace', false);
+    raintpl::configure('debug', true);
 
-	$tpl = new raintpl(); //include Rain TPL
+    $tpl = new raintpl(); //include Rain TPL
 
-	$tpl->assign( "url", $url);
-	$tpl->assign( "tinyurl", md5($url));
-	$tpl->assign( "title", $title);
+    $tpl->assign("url", $url);
+    $tpl->assign("tinyurl", md5($url));
+    $tpl->assign("title", $title);
 
-    $tpl->assign( "isLogged", Session::isLogged());
+    $tpl->assign("isLogged", Session::isLogged());
     if (Session::isLogged()) {
-        $tpl->assign( "username", $_SESSION['username']);
-        $tpl->assign( "logpage", "./log.php?logout");
-        $tpl->assign( "logname", "Logout");
+        $tpl->assign("username", $_SESSION['username']);
+        $tpl->assign("logpage", "./log.php?logout");
+        $tpl->assign("logname", "Logout");
     } else {
-        $tpl->assign( "logpage", "./log.php");
-        $tpl->assign( "logname", "Login");
+        $tpl->assign("logpage", "./log.php");
+        $tpl->assign("logname", "Login");
     }
 
-    $tpl->assign( "content", $content);
-	$tpl->assign( "version", VERSION);
-	
-	$tpl->draw( "article"); // draw the template
+    $tpl->assign("content", $content);
+    $tpl->assign("version", VERSION);
+
+    $tpl->draw("article"); // draw the template
 }
 
-if(isset($_GET['url']) && $_GET['url'] != null && trim($_GET['url']) != "") {
-	// get url link
-	if(strlen(trim($_GET['url'])) > 2048) {
-		echo "Error URL is too large !!";
-	} else {
-		$url = trim($_GET['url']);
+if (isset($_GET['url']) && $_GET['url'] != null && trim($_GET['url']) != "") {
+    // get url link
+    if (strlen(trim($_GET['url'])) > 2048) {
+        echo "Error URL is too large !!";
+    } else {
+        $url = trim($_GET['url']);
 
-        if(!Utils::isValidMd5($url)) {
-    		// decode it
-    		$url = html_entity_decode($url);
-    		
-    		// if url use https protocol change it to http
-    		if (!preg_match('!^https?://!i', $url)) $url = 'http://'.$url;
+        if (!Utils::isValidMd5($url)) {
+            // decode it
+            $url = html_entity_decode($url);
+
+            // if url use https protocol change it to http
+            if (!preg_match('!^https?://!i', $url))
+                $url = 'http://' . $url;
         }
 
         $article = new Article;
         $article->setUrl($url);
 
-//        if(!$article->isAlreadyExists()) {
-            if($article->retrieveContent()) {
-                if($article->readiIt()) {
-                    $article->modifyContent();
+        if (!$article->isAlreadyExists()) {
+            if ($article->retrieveContent()) {
+                if ($article->readiIt(isset($_GET['debug']))) {
+                    //$article->modifyContent();
                     //$article->saveContent();
                 }
             }
-/*
         } else {
             $article = Article::getArticle($url);
-                    // only for debug
-                if($article->readiIt()) {
-                    $article->modifyContent();
-                    $article->saveContent();
-                }
+            // only for debug
+            if ($article->readiIt(isset($_GET['debug']))) {
+                //$article->modifyContent();
+                $article->saveContent();
+            }
         }
-*/
 
-        if($article && $article->isLoaded()) {
-				generate_page($article->getUrl(),$article->getTitle(),$article->getFinalContent());
-				//generate_page($url,$r->articleTitle->innerHTML,$r->articleContent->innerHTML);
-		} else {
-			echo "Error unable to get link : ".$url;
-		}
-	}
+        if ($article && $article->isLoaded()) {
+            generate_page($article->getUrl(), $article->getTitle(), $article->getFinalContent());
+            //generate_page($url,$r->articleTitle->innerHTML,$r->articleContent->innerHTML);
+        } else {
+            echo "Error unable to get link : " . $url;
+        }
+    }
 }
-?>
